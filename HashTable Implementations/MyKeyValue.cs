45fb763d45efbfade6934ad43bdab17d72ede4a0
Mohammad <MohammadRasooli.j@gmail.com>
2025-09-03 22:07:00 +0330

    docs: Document the MyKeyValue class properties and constructor
    Added comments to explain the purpose of the Key and Value properties, as well as the constructor for the key-value pair data structure.

--- HashTable Implementations/MyKeyValue.cs
+++ HashTable Implementations/MyKeyValue.cs
@@ -1,8 +1,13 @@
+// Represents a generic key/value pair.
 public class MyKeyValue<TKey, TValue>
 {
+    // Gets or sets the key in the key/value pair.
     public TKey Key { get; set; }
+
+    // Gets or sets the value in the key/value pair.
     public TValue Value { get; set; }
 
+    // Initializes a new instance of the MyKeyValue class with the specified key and value.
     public MyKeyValue(TKey key, TValue value)
     {
         Key = key;
